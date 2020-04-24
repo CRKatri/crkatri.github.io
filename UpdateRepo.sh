a7e86ba932ad976e2d92e37d59c763004a68356b
CRKatri <katri.cameron@gmail.com>
2020-04-24 01:40:54 -0400

    Fixed Commit Messages

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -7,7 +7,7 @@ echo 'Message for commit?'
 read commitMessage
 
 echo 'Commiting'
-git commit -m '$commitMessage'
+git commit -m "$commitMessage"
 
 echo 'Pushing to github'
 git push -u origin master
