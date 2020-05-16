50707905211f015364006da91d2af44434577d7f
CRKatri <katri.cameron@gmail.com>
2020-05-16 00:24:04 -0400

    That didnt fix it

--- loader/redirect_requests.py
+++ loader/redirect_requests.py
@@ -1,5 +1,5 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
-    if flow.request.pretty_host == "assets.checkra.in/loader/config.json":
-        flow.request.host = "crkatri.github.io/loader/config.json"
+    if flow.request.pretty_host == "https://assets.checkra.in/loader/config.json":
+        flow.request.host = "https://crkatri.github.io/loader/config.json"
