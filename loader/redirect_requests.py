e40e18faed001e9a935f92ed32cfb9e37516a8e9
CRKatri <katri.cameron@gmail.com>
2020-05-16 00:21:41 -0400

    Whoops, I didnt mean to redirect everything

--- loader/redirect_requests.py
+++ loader/redirect_requests.py
@@ -1,5 +1,5 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
-    if flow.request.pretty_host == "assets.checkra.in":
-        flow.request.host = "crkatri.github.io"
+    if flow.request.pretty_host == "assets.checkra.in/loader/config.json":
+        flow.request.host = "crkatri.github.io/loader/config.json"
