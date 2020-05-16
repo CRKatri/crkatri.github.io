5ebc9c706c09b0c7594ccd1cbe2ab7a8516d2081
CRKatri <katri.cameron@gmail.com>
2020-05-16 00:32:02 -0400

    I broke this

--- loader/redirect_requests.py
+++ loader/redirect_requests.py
@@ -1,5 +1,5 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
-    if flow.request.pretty_host == "https://assets.checkra.in":
-        flow.request.host = "https://crkatri.github.io"
+    if flow.request.pretty_host == "assets.checkra.in":
+        flow.request.host = "crkatri.github.io"
