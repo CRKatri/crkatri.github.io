251ec1903ad5bd2fd80008e5ea9b12b251b0dad2
CRKatri <katri.cameron@gmail.com>
2020-06-18 12:20:41 -0400

    Switch to apt-ftparchive, zstd, and xz. Add Don't Kill Termius

--- UpdatePackages.sh
+++ UpdatePackages.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-rm Packages
-dpkg-scanpackages --multiversion ./debs > Packages
-rm Packages.gz
-rm Packages.bz2
-bzip2 -9fk Packages
-gzip -9fk Packages
+apt-ftparchive packages ./debs/ > Packages
+xz -c9 Packages > Packages.xz
+zstd -q -c19 Packages > Packages.zst
