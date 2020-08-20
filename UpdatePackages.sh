d64b637e599c52c94a46f80f8bb2c3911a466c1b
Cameron Katri <katri.cameron@gmail.com>
2020-08-20 10:55:17 -0400

    Secure it

--- UpdatePackages.sh
+++ UpdatePackages.sh
@@ -3,4 +3,8 @@
 apt-ftparchive packages ./debs/ > Packages
 xz -c9 Packages > Packages.xz
 zstd -q -c19 Packages > Packages.zst
+
+apt-ftparchive release -c config/iphoneos-arm64.conf . > \
+	Release 2>/dev/null
+
 gpg -abs -u 7D3B36CEA40FCC2181FB6DCDBAFFD97826540F1C -o Release.gpg Release
