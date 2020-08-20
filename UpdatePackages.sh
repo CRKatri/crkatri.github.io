80ab5fb24c5cadda99fece47baef91535e8fa588
Cameron Katri <katri.cameron@gmail.com>
2020-08-20 03:09:50 -0400

    Sign it

--- UpdatePackages.sh
+++ UpdatePackages.sh
@@ -3,3 +3,4 @@
 apt-ftparchive packages ./debs/ > Packages
 xz -c9 Packages > Packages.xz
 zstd -q -c19 Packages > Packages.zst
+gpg -abs -u 7D3B36CEA40FCC2181FB6DCDBAFFD97826540F1C -o Release.gpg Release
