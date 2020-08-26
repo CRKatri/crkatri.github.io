bce1f88de38c77a0761c90ca35658f63307aa3b1
Cameron Katri <katri.cameron@gmail.com>
2020-08-26 19:04:30 -0400

    Update

--- UpdatePackages.sh
+++ UpdatePackages.sh
@@ -7,4 +7,5 @@ zstd -q -c19 Packages > Packages.zst
 apt-ftparchive release -c release.conf . > \
 	Release 2>/dev/null
 
+rm Release.gpg
 gpg -abs -u 7D3B36CEA40FCC2181FB6DCDBAFFD97826540F1C -o Release.gpg Release
